0faa685c71149702eeea5f4bad29f81845604fc2
chris reynolds <chris@jazzsequence.com>
2012-08-11 12:35:19 -0600

    adds `option-setup.php` -- this is where all the markup for the options will live as well as the function used to display them
    #gklst

---
+++ inc/option-setup.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Do Product option stuff
+ * @author Chris Reynolds
+ * @since 0.3
+ * loads the options
+ */
+function ap_products_do_options() {
+	$options_before = '<table class="form-table">';
+	$options_after = '</table>';
+
+	echo $options_before;
+	// do stuff
+	echo $options_after;
+}
+?>
