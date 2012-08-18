efdb85ed2dd6ee9355447120c385dc7151391282
chris reynolds <chris@jazzsequence.com>
2012-08-18 12:10:25 -0600

    switching to f/gitweb branch of Github Updater class

--- products.php
+++ products.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Products
-Plugin URI: http://arcanepalette.com
+Plugin URI: https://github.com/jazzsequence/Products
 Description: A simple shop plugin based on custom post types with integrated support for Cart66
 Version: 0.5
 Author: Arcane Palette Creative Design
@@ -28,24 +28,8 @@ License: GPL3
 	define('product_plugin_path', WP_PLUGIN_URL . '/products/');
 	define('product_plugin_dir', WP_PLUGIN_DIR . '/products/');
 	define('product_plugin_images', product_plugin_path . 'images/');
+	include_once( product_plugin_dir . 'inc/updater.php' );
 
-
-include_once( product_plugin_dir . 'inc/updater.php' );
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-    $config = array(
-        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-        'proper_folder_name' => 'products', // this is the name of the folder your plugin lives in
-        'api_url' => 'https://api.github.com/repos/jazzsequence/Products', // the github API url of your github repo
-        'raw_url' => 'https://raw.github.com/jazzsequence/Products/master', // the github raw url of your github repo
-        'github_url' => 'https://github.com/jazzsequence/Products/', // the github url of your github repo
-        'zip_url' => 'https://github.com/jazzsequence/Products/zipball/master', // the zip url of the github repo
-        'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-        'requires' => '2.9', // which version of WordPress does your plugin require?
-        'tested' => '3.4.1', // which version of WordPress is your plugin tested up to?
-        'readme' => 'readme.md' // which file to use as the readme for the version number
-    );
-	new WPGitHubUpdater($config);
-}
 
 /**
  * Products Post Type
